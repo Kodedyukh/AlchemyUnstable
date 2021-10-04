5061949aeec8ad07fb0b3c45514762d2c41b9b29
Stanislav Korol <aprioriface@gmail.com>
2021-10-04 21:58:53 +0300

    add audio manager

---
+++ assets/scripts/Enums/AudioTypes.js
@@ -0,0 +1,11 @@
+export default cc.Enum({
+    None: 0,
+    BenchExplosion: 1,
+    BottleScratch: 2,
+    Cat: 3,
+    Fanfare: 4,
+    Glass: 5,
+    MagicStoutFarm: 6,
+    Mumble: 7,
+    PutBottle: 8
+});
